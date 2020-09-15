1904041a5af6d336aee17d51252c8063cba4771e
Jade Kim <jade@buzlink.io>
2020-09-15 17:37:19 +0900

    Release v0.0.2
     - Modify fromJson non-nullable

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val artifactName = "gson-kotlin"
 val artifactGroup = "kr.jadekim"
-val artifactVersion = "0.0.1"
+val artifactVersion = "0.0.2"
 group = artifactGroup
 version = artifactVersion
 
